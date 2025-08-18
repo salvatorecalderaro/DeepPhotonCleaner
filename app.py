dfc7f6f9fe898fc0278d4a5980ac0881cd6a251b
Salvatore Calderaro <salvatore.calderaro01@unipa.it>
2025-08-18 17:49:25 +0200

    adding file uploader

--- app.py
+++ app.py
@@ -7,3 +7,14 @@ st.write(
     "Upload a `.fits` file to clean noisy photon bins and download the cleaned result."
 )
 
+
+uploaded_file = st.file_uploader(
+    "📂 Upload a FITS file",
+    type=["fits"],
+    key=st.session_state.get("uploader_key", "default_uploader"),
+)
+
+if "uploaded_filename" not in st.session_state and not uploaded_file:
+    st.info("Please upload a `.fits` file to start.")
+    st.stop()
+
